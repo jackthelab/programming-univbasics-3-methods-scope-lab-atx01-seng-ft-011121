4bfa3f12bcdc8c385da1f67f17a7cbce0b7ae30d
Jacob Hampton <jhampton792@gmail.com>
2020-12-08 03:36:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,5 @@
-$status = "Thank you Mario! But Our Princess Is In Another Castle!"
-
 def mario
-  #status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
